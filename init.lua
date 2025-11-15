69f461296fa60e743281aecf8eb211fd9f60c1be
ruei <ruei@local.null>
2025-11-15 17:02:13 +0800

    update scrolloff

--- init.lua
+++ init.lua
@@ -73,7 +73,7 @@ vim.o.inccommand = 'split'
 vim.o.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
-vim.o.scrolloff = 9999
+vim.o.scrolloff = 8
 
 -- tabsize
 vim.o.shiftwidth = 2
@@ -904,7 +904,7 @@ require('lazy').setup({
   -- require 'kickstart.plugins.debug',
   -- require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
+  require 'kickstart.plugins.autopairs',
   -- require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
